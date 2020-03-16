53fa47a1af685ac8d5f8a6bca33ce4fbeaa558a0
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-15 19:49:02 -0500

    Set up method to return Hello Spring!

--- src/main/java/org/launchcode/hellopractice/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellopractice/controllers/HelloController.java
@@ -1,8 +1,19 @@
 package org.launchcode.hellopractice.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 //Controller lets springboot know this will handle http requests
 @Controller
 public class HelloController {
+
+    //tells springboot this will return plain text not html
+    //getmapping will only accept get requests
+    @GetMapping
+    @ResponseBody
+    public String hello(){
+        return "Hello, Spring!";
+    }
+
 }
