4a8c1f03ae634bc37ca8a5bd28920761757e4f09
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-15 19:43:51 -0500

    Created HelloController

---
+++ src/main/java/org/launchcode/hellopractice/controllers/HelloController.java
@@ -0,0 +1,8 @@
+package org.launchcode.hellopractice.controllers;
+
+import org.springframework.stereotype.Controller;
+
+//Controller lets springboot know this will handle http requests
+@Controller
+public class HelloController {
+}
