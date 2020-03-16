d38b7bc792e0930c290a6059f57a368271ba4226
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-15 20:02:27 -0500

    Set up a dynamic response handler

--- src/main/java/org/launchcode/hellopractice/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellopractice/controllers/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.hellopractice.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //Controller lets springboot know this will handle http requests
@@ -10,14 +11,27 @@ public class HelloController {
 
     //tells springboot this will return plain text not html
     //getmapping will only accept get requests
+    //handles requests at path /hello
+/*
     @GetMapping("hello")
     @ResponseBody
     public String hello(){
         return "Hello, Spring!";
     }
+*/
+
+    //handles requests at path /goodbye
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
+
+    //handles request of the form /hello?name=variable
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
 }
