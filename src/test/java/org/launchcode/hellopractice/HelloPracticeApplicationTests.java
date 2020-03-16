61479ad160a4fc07044cb835be710da7ebeb431b
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-15 19:28:22 -0500

    Initial commit

---
+++ src/test/java/org/launchcode/hellopractice/HelloPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
