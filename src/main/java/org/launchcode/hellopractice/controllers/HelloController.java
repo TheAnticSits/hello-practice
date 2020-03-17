5046d5d4f19e42c963d524f99c84c972ac6e48ac
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-16 22:07:42 -0500

    Changed hello controller to use the form.html

--- src/main/java/org/launchcode/hellopractice/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellopractice/controllers/HelloController.java
@@ -5,54 +5,29 @@ import org.springframework.web.bind.annotation.*;
 
 //Controller lets springboot know this will handle http requests
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
-    //tells springboot this will return plain text not html
-    //getmapping will only accept get requests
-    //handles requests at path /hello
-/*
-    @GetMapping("hello")
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public String hello(){
-        return "Hello, Spring!";
-    }
-*/
-    // lives at /hello/goodbye because of inherited requestmappping.
-    //handles requests at path /goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring!";
+    public String hello(@RequestParam String name){
+        return "Hello, " + name + "!";
     }
 
-    //handles request of the form /hello?name=variable
-    //with requestmapping it accepts both Get and Post requests
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
-    //handles requests of the form /hello/launchcode
-    //path parameter
-    //@pathvariable tells springboot that this handler method is looking for a request to /hello/name and name can be anything
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
+    //responds to /hello/Launchcode
+    @RequestMapping("hello/{name}")
+    @ResponseBody
+    public String helloAgain(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
-    //lives at /hello/form
-    //a form is by default get
     @GetMapping("form")
-    @ResponseBody
     public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" + //submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body" +
-                "<html>";
+        return "form";
     }
 }
